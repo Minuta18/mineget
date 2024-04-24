339584d4ae4a567a80368b7aed739e550af8de5a
Minuta18 <ig.samsonov10@yandex.ru>
2024-04-24 13:41:27 +0300

    First tests

---
+++ mineget/download.py
@@ -0,0 +1,5 @@
+from mineget import Loader
+
+class Downloader:
+    def __init__(self): pass
+    def download(self, name: str, version: str, loader: Loader) -> None: pass
---
+++ mineget/loader.py
@@ -0,0 +1,7 @@
+import enum
+
+class Loader(enum.Enum):
+    Forge = 'forge'
+    Fabric = 'fabric'
+    Quilt = 'quilt'
+    Neoforge = 'neoforge'
